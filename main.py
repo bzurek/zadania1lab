554ec41dd9be278265f21b60f1c632dd2c3f05fa
Barbara Zurek <barbara.zurek999@gmail.com>
2021-02-22 14:27:16 +0100

    1, 2, 3 zadania

---
+++ main.py
@@ -0,0 +1,39 @@
+# 1 zad
+
+# a = 10
+# b = 5
+# c = 3.14
+# d = 5.6
+# e = (2+3j)
+# f = (5+6j)
+# g = Ala
+# h = Kot
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+# print(g)
+# print(h)
+
+# 2 zad
+# a, b, c = 25, 5, 2
+# dodawanie = a + b
+# odejmowanie = a - b
+# mnozenie = a * b
+# dzielenie = a/b
+
+# # 3 zad
+# a += b
+# print(a)
+# a -= c
+# print(a)
+# b *= c
+# print(b)
+# a /= b
+# print(a)
+# b **=c
+# print(b)
+# a %=c
+# print(a)
